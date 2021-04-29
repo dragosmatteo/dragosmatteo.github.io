5debcf222be4b280535a02c5bd2360236996f4c1
Dragoș Mateo Dămoc <56765258+dragosmatteo@users.noreply.github.com>
2021-04-29 09:13:20 +0300

    Update script.js

--- guessmynumber/script.js
+++ guessmynumber/script.js
@@ -1,19 +1,4 @@
-// 'use strict';
-
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.message').textContent = 'Correct answer !';
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.score').textContent = 20;
-// // document.querySelector('.number').textContent = ;
-
-// document.querySelector('.guess').value = 3;
-// console.log(document.querySelector('.guess').value);
-
-// const x = function(){
-//     console.log(23);
-// }
+'use strict';
 
 let numarRandom = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
@@ -26,10 +11,9 @@ const displayMessage = function(message){
 document.querySelector('.check').addEventListener('click',function(){
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
-
     // When there is no input
     if(!guess){
-        displayMessage('Introduce a valid number!🔥');
+        displayMessage('Introduce a valid number!');
     }
 
     // When is equal
@@ -56,7 +40,7 @@ document.querySelector('.check').addEventListener('click',function(){
          if(score > 1){
        // document.querySelector('.message').textContent =
       // guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
-             displayMessage(guess > numarRandom ? 'Too high! 📈' : 'Too low 📉') ; // Scurta si decizie la atribuire daca este indeplinita conditia
+             displayMessage(guess > numarRandom ? 'Too high!' : 'Too small!') ; 
              score--;
              document.querySelector('.score').textContent = score;}
          else { 
@@ -64,34 +48,7 @@ document.querySelector('.check').addEventListener('click',function(){
            document.querySelector('.score').textContent = 0; }
     }
 });
-
-// Formula lunga
-
-    // Whan my number introduced is grater
-//     else if(guess > numarRandom){
-//         if(score > 1){
-//              document.querySelector('.message').textContent = 'Too high!';
-//              score--;
-//              document.querySelector('score').textContent = score;}
-//          else { 
-//              document.querySelector('.message').textContent = 'You lost game!';
-//              document.querySelector('.score').textContent = 0;}
-//     }
-
-//     // Whan my number introduced is smaller
-//     else if(guess < numarRandom){
-//         if(score > 1){
-//              document.querySelector('.message').textContent = 'To low !';
-//              score--;
-//              document.querySelector('.score').textContent = score;}
-//      else {
-//          document.querySelector('.message').textContent = 'You lost game!';
-//          document.querySelector('.score').textContent = 0;}
-//      }
-// });
-
-
-// Challenge 
+ 
 // Again button aducem totul de la 0 
 
 document.querySelector('.again').addEventListener('click', function() {
@@ -106,7 +63,4 @@ document.querySelector('.again').addEventListener('click', function() {
      document.querySelector('.number').style.background ='#eee';
      document.querySelector('.number').style.borderRadius = '0px';
      //  document.querySelector('.highscore').textContent = 0;
-}); // te las :)
-
-// merge acum deci era de la score? trebuie declarat neaparat din nou? n am inteles
-// nu trebuie declarat. este declarat o singura data, sus. ii resetezi valoarea aiciaaa
+})
